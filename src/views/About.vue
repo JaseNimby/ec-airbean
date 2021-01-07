ab08f717611366cddaab8eadfe4b13e342a0ef89
JaseNimby <jasenimby@yahoo.co.uk>
2021-01-07 16:34:05 +0100

    felsökning

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,5 @@
-<template><div>Om Oss</div></template>
-
-<script>
-export default {
-  name: "About",
-};
-</script>
-
-<style scoped>
-</style>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,18 @@
 <template>
-  <div class="wrapper">
-    <img src="../assets/graphics/intro-graphic-left.svg" class="left" />
-    <div class="center">
-      <img
-        src="../assets/graphics/airbean-landing.svg"
-        class="logo"
-        @click="$router.push('nav')"
-      />
-    </div>
-    <img src="../assets/graphics/intro-graphic-right.svg" class="right" />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
-  name: "Home",
-};
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
 </script>
-
-<style >
-body {
-  background-color: rgb(51, 160, 127);
-  margin: 0;
-}
-
-.wrapper {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-}
-.left {
-  grid-column-start: 1;
-}
-.right {
-  grid-column-start: 4;
-}
-
-.center {
-  grid-column-start: 2;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.logo {
-  cursor: pointer;
-}
-</style>
