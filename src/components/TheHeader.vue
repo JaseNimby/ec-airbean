33067ad3ef904b9252be3ae10bd844db8c5a9244
JaseNimby <jasenimby@yahoo.co.uk>
2021-01-07 23:10:13 +0100

    about page done

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>
-    <TheHamburger />
-  </div>
+  <section class="background">
+    <div class="nav">
+      <img src="../assets/graphics/navicon.svg" />
+    </div>
+  </section>
 </template>
 
 <script>
-import TheHamburger from "@/components/TheHamburger";
 export default {
   name: "Header",
-  components: { TheHamburger },
 };
 </script>
 
+
+
 <style scoped>
+.background {
+  background-image: url("../assets/graphics/graphics-header.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 210px;
+}
+
+.nav {
+  background-color: rgb(255, 252, 249);
+  width: 5%;
+  height: 5%;
+  position: relative;
+  top: 5%;
+  left: 5%;
+  cursor: pointer;
+  padding: 10px;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  border-radius: 50%;
+}
 </style>
